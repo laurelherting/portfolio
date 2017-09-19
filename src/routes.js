9d678f4c7c09e9c8b2168fd9e6f16d5934b63cd0
Laurel Herting <laurel.herting@gmail.com>
2017-09-18 20:30:47 -0600

    WIP route

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import {
   BrowserRouter,
-  Routes
+  Router
 } from 'react-router-dom';
 import Main from './src/components/main';
 import Home from './src/app';
 import NotFound from './components/errors/not_found';
+import IndexRoute from 'ReactRouter.IndexRoute';
+import config from 'react-global-configuration';
 
-class Routes extends Component {
+class Routes extends React.Component {
   render() {
         return (
+          <div>
+          <div>{ config.get('foo') }</div>
+          {/* console.log('Routes', Routes); */}         
             <Router history={browserHistory}>
                 <Route path='/' component={Main}>
-                    <IndexRoute config={config} component={Home}/>
+                    <IndexRoute config={config} component={App}/>
+          {/* console.log('Routes ', JSON.stringify(Routes)); */}
                     <Route path='*' component={NotFound}/>
                 </Route>
-            </Router>
+            </Router> 
+          </div>
         );
     }
 };
