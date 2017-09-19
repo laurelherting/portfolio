dd496a35f66e95cc17f42075806734f01a969716
Laurel Herting <laurel.herting@gmail.com>
2017-09-19 15:16:40 -0600

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <div className="App-header">
           <h2>Laurel Herting</h2>
@@ -10,6 +12,7 @@ class App extends Component {
         <p className="App-intro">
         </p>
       </div>
+      </Router>
     );
   }
 }
