2d4c916e6b2e544ef0972f4e83a05c253a5525fe
Laurel Herting <laurel.herting@gmail.com>
2017-09-21 18:31:33 -0600

    resize image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import background from './assets/background.JPG';
+import Image from 'react-image-resizer';
 
 console.log(background); //background.JPG
 
@@ -10,7 +11,7 @@ class App extends Component {
       <Router>
       <div className="App">
         <div className="App-header">
-          <img src={background} alt="" class="Background"/>
+          <img src={background} alt="" class="Background" width={1297} height={710} />
           <h2>Laurel Herting</h2>
         </div>
         <p className="App-intro">
