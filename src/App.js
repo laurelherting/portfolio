b3ef73a2a21ec2f55915d6eb8f2685fc68bc0b20
Laurel Herting <laurel.herting@gmail.com>
2017-09-22 13:01:52 -0600

    add navigation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import background from './assets/background.JPG';
 import Image from 'react-image-resizer';
-
-console.log(background); //background.JPG
+// import Link from './link';
 
 class App extends Component {
   render() {
@@ -12,6 +11,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={background} alt="" class="Background" width={1297} height={710} />
+          <nav id="nav-wrap" class="opaque">
+          </nav>
           <h2>Laurel Herting</h2>
         </div>
         <p className="App-intro">
