75d40fd576cb8e4961040c9cfd8280710b769621
Laurel Herting <laurel.herting@gmail.com>
2017-09-22 14:37:00 -0600

    change case

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import background from './assets/background.JPG';
+import background from './assets/background.jpg';
 import Image from 'react-image-resizer';
 // import Link from './link';
 
@@ -10,7 +10,7 @@ class App extends Component {
       <Router>
       <div className="App">
         <div className="App-header">
-          <img src={background} alt="" class="Background" width={1297} height={710} />
+          <img src={background} alt="" className="Background" width={1297} height={710} />
           <nav id="nav-wrap" class="opaque">
           </nav>
           <h2>Laurel Herting</h2>
