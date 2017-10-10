29d2a45bfbcef712357088ead301882200a10c82
Laurel Herting <laurel.herting@gmail.com>
2017-10-09 19:34:37 -0600

    update syntax

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import ReactRouter from 'react-router';
 
 import config from './Config';
 
 import Main from './components/Main';
 import Home from './components/Home';
 
+import Router from ReactRouter.Router;
+import Route from ReactRouter.Route;
 import IndexRoute from ReactRouter.IndexRoute;
 import { BrowserRouter, Route, browserHistory } from 'react-router-dom'
 
