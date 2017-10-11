514a52155020f15930518db30588e665730d7fb7
Laurel Herting <laurel.herting@gmail.com>
2017-10-10 19:15:22 -0600

    edit render function

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,49 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from React.PropTypes;
 
 const Loading = () => (
-    propTypes: {
-        text: PropTypes.string,
-        speed: PropTypes.number
-    },
-
-    getDefaultProps: () => {
-        return {
-            text: 'Loading',
-            speed: 300
-        };
-    },
-
-    getInitialState: () => {
-        this.originalText = this.props.text;
-        return {
-            text: this.originalText
-        };
-    },
-
-    componentDidMount: () => {
-        const stopper = this.originalText + '...';
-        this.interval = setInterval(() => {
-            if (this.state.text === stopper) {
-                this.setState({
-                    text: this.originalText
-                });
-            } else {
-                this.setState({
-                    text: this.state.text + '.'
-                });
-            }
-        }.bind(this), this.props.speed);
-    },
-
-    componentWillUnmount: () => {
-        window.clearInterval(this.interval);
-    },
-
     render: () => {
         return (
             <div className='loading'>
-                <p className='loading'>{this.state.text}</p>
             </div>
         );
     }
