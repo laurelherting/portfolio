8e6f5f445650ed492d38c3a0ffa1a4c07adab8fc
Laurel Herting <laurel.herting@gmail.com>
2017-10-11 17:13:56 -0600

    change syntax, update props

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -7,25 +7,35 @@ import Work from './work';
 import Education from './education';
 import Footer from './footer';
 
-class Section extends React.createClass {
+const Section = () => (
     propTypes: {
         basics: ResumePropTypes.basics,
         work: ResumePropTypes.work_set,
-        education: ResumePropTypes.education_set
+        education: ResumePropTypes.education_set,
+        skills: ResumePropTypes.skills_set,
+        languages: ResumePropTypes.languages_set,
+        references: ResumePropTypes.references_set,
+        changeWaypoint: PropTypes.func.isRequired
     },
 
     render: () => {
+        const skills_content = {
+            skills: this.props.skills,
+            languages: this.props.languages
+        };
         return (
             <div>
                 <About content={this.props.basics}/>
                 <Resume>
                     <Work content={this.props.work}/>
-                   <Education content={this.props.education}/>
+                    <Education content={this.props.education}/>
+                    <Skills={this.props.skills}/>
                 </Resume>
+                <References={this.props.references}/>
                 <Footer content={this.props.basics}/>
             </div>
         );
     }
-};
+);
 
 export default Section;
