1b28a17f88e0386a223c9f979e193732ab8d0f75
Laurel Herting <laurel.herting@gmail.com>
2017-10-26 19:06:03 -0600

    edit nav-wrap-mobile button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => (
 <Router>
     <div>
         <header id="home" className="height: 753px;">
+            ::before
             <nav id="nav-wrap" className="opaque">
                 <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
                 ::before
@@ -31,36 +32,39 @@ const App = () => (
                     </ul>
                 </div>
             </nav>
-            <div className="banner-text">
-                <h2 className="responsive-headline">Laurel Herting</h2>
-            </div>
+            <div className="row banner">
+                ::before
+                <div className="banner-text">
+             <h2 className="responsive-headline">Laurel Herting</h2>
+                </div>
 
-            <div>
-                <ul className="social">
-                    <li>
-                        <a href="skype: +xxxxxxxxxx">
-                            <i className="fa fa-phone" />
-                        </a>
-                        <a href="mailto:-.-@gmail.com">
-                            <i className="fa fa-envelope" />
-                        </a>
-                        <a href="https://linkedin.com/in/laurelherting">
-                            <i className="fa fa-linkedin" aria-hidden="true" />
-                        </a>
-                        <a href="https://github.com/laurelherting">
-                            <i className="fa fa-github" aria-hidden="true" />
-                        </a>
-                        <a href="https://stackoverflow.com/users/8473290/lherting">
-                            <i className="fa fa-stack-overflow" />
-                        </a>
-                        <a href="https://www.quora.com/profile/Laurel-Herting">
-                            <i className="fa fa-quora" />
-                        </a>
-                        <a href="https://hub.docker.com/u/lherting">
-                            <i className="fa fa-docker" />
-                        </a>
-                    </li>
-                </ul>
+                <div>
+                    <ul className="social">
+                        <li>
+                            <a href="skype: +xxxxxxxxxx">
+                                <i className="fa fa-phone" />
+                            </a>
+                            <a href="mailto:-.-@gmail.com">
+                                <i className="fa fa-envelope" />
+                            </a>
+                            <a href="https://linkedin.com/in/laurelherting">
+                                <i className="fa fa-linkedin" aria-hidden="true" />
+                            </a>
+                            <a href="https://github.com/laurelherting">
+                                <i className="fa fa-github" aria-hidden="true" />
+                            </a>
+                            <a href="https://stackoverflow.com/users/8473290/lherting">
+                                <i className="fa fa-stack-overflow" />
+                            </a>
+                            <a href="https://www.quora.com/profile/Laurel-Herting">
+                                <i className="fa fa-quora" />
+                            </a>
+                            <a href="https://hub.docker.com/u/lherting">
+                                <i className="fa fa-docker" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </header>
     </div>
