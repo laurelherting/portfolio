cc3008580dde79e46cc9aa769b31ba81a5e369fb
Laurel Herting <laurel.herting@gmail.com>
2017-10-30 19:37:57 -0600

    import font-awesome/css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'font-awesome/css/font-awesome.css';
+// import 'fontello/css/fontello.css';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
