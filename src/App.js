b35c1678cd21b10e042f5ccb40d413b96e428caf
Laurel Herting <laurel.herting@gmail.com>
2017-11-17 15:06:01 -0700

    text edit-about me section

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ const App = () => (
                     <div className="ten columns main-col">
                         <h3>About Me</h3>
                         <p>
-                            Diligent, results-driven, detailed, and articulate software developer with participation creating solid application architecture and coding. Passionate about problem-solving in order to understand abstact concepts through strong research skills resulting in innovative solutions.
+                            Passionate, diligent, results-driven, detailed, and articulate software developer with participation creating solid application architecture and coding. Passionate about problem-solving in order to understand abstact concepts through strong research skills resulting in innovative solutions.
                         </p>
                         <p>
                         Excellent written and verbal communication skills while maintaining a positive attitude. Great at time and task management, quick learning ability for deep and broad technical exposure. Confident, up-to-date team player determined to achieve high end user focus.
