320abac94478873a98bd72cdd246abe1632690c2
Laurel Herting <laurel.herting@gmail.com>
2017-11-22 15:54:53 -0700

    remove footer

--- src/App.js
+++ src/App.js
@@ -135,35 +135,6 @@ const App = () => (
                     </div>
                 </div>
             </section>
-            <footer>
-                <div className="twelve columns">
-                    <ul className="social-links">
-                        <li>
-                            <a href="skype: +xxxxxxxxxx">
-                                <i className="fa fa-phone" />
-                            </a>
-                            <a href="mailto:-.-@gmail.com">
-                                <i className="fa fa-envelope" />
-                            </a>
-                            <a href="https://linkedin.com/in/laurelherting">
-                                <i className="fa fa-linkedin" aria-hidden="true" />
-                            </a>
-                            <a href="https://github.com/laurelherting">
-                                <i className="fa fa-github" aria-hidden="true" />
-                            </a>
-                            <a href="https://stackoverflow.com/users/8473290/lherting">
-                                <i className="fa fa-stack-overflow" />
-                            </a>
-                            <a href="https://www.quora.com/profile/Laurel-Herting">
-                                <i className="fa fa-quora" />
-                            </a>
-                            <a href="https://hub.docker.com/u/lherting">
-                                <i className="fa fa-docker" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </footer>
         </div>
     </div>
 </Router>
