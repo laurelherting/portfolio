25cf8c3c3b236089ce08a978cc0db0b1c364f6d7
Evan Harmon <evan.p.harmon@gmail.com>
2017-11-24 08:37:06 -0800

    fix(lint): absolute before relative imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'font-awesome/css/font-awesome.css';
-// import 'fontello/css/fontello.css';
+import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
