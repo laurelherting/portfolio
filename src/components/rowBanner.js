95bc896e3a461538420fe99586dfe2bcff84b77d
Laurel Herting <laurel.herting@gmail.com>
2018-01-30 09:41:30 -0700

    create new folder to seperate out components

---
+++ src/components/rowBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const rowBanner = () => (
+  <div className="row banner">
+    <div className="banner-text">
+      <h2 className="responsive-headline">Laurel Herting</h2>
+    </div>
+  </div>
+);
+
+export default rowBanner;
