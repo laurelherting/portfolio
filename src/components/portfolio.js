bbfef32c937328122d7072131f0f28f5396c3bf2
Laurel Herting <laurel.herting@gmail.com>
2018-05-31 10:29:13 -0600

    update classNames

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -14,24 +14,21 @@ const Portfolio = () => (
         <p>React Single-Page App</p>
       </a>
       <br />
-      <div className="reactTransitions">
-        <a
-          href="https://react-full-stack-list.herokuapp.com/"
-          title="Click to view Heroku deployed app"
-        >
-          <p>React Full Stack App</p>
-        </a>
-        <p>app</p>
-        <br />
-        <div className="season-quotes">
-          <a
-            href="https://season-quotes.herokuapp.com/"
-            title="Click to view Heroku deployed app"
-          >
-            <p>ExpressJS React App</p>
-          </a>
-        </div>
-      </div>
+      <div className="reactTransitions" />
+      <a
+        href="https://react-full-stack-list.herokuapp.com/"
+        title="Click to view Heroku deployed app"
+      >
+        <p>React Full Stack App</p>
+      </a>
+      <br />
+      <div className="season-quotes" />
+      <a
+        href="https://season-quotes.herokuapp.com/"
+        title="Click to view Heroku deployed app"
+      >
+        <p>ExpressJS React App</p>
+      </a>
     </section>
   </div>
 );
